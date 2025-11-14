94336c8280417dc1a185dce2f5cf5bc812926b4f
pietro <p.celeste@osynapsy.net>
2025-11-14 10:55:41 +0100

    Added documentation

--- src/FormCsrf.php
+++ src/FormCsrf.php
@@ -4,6 +4,24 @@ namespace Osynapsy\Csrf;
 use Osynapsy\Html\Component\InputHidden;
 use Osynapsy\Html\Tag;
 
+/**
+ * Helper class to apply CSRF protection fields to Osynapsy forms.
+ *
+ * This class provides a static method `apply()` which generates a CSRF nonce
+ * and token using the `Token` class and adds them as hidden input fields
+ * (`csrf_nonce` and `csrf_token`) to a given form component.
+ *
+ * Usage:
+ *   $form = new MyForm();
+ *   \Osynapsy\Csrf\FormCsrf::apply($form, $secretKey);
+ *
+ * The hidden fields added by this helper should be validated using
+ * `Osynapsy\Csrf\Action\AbstractAction::checkCsrf()` in the action
+ * handling the form submission.
+ *
+ * @package Osynapsy\Csrf
+ * @author Pietro Celeste <p.celeste@osynapsy.net>
+ */
 class FormCsrf
 {
     const FIELD_NONCE = 'csrf_nonce';
